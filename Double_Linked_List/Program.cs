fbb8ff6cebcd66439f4a566b1d7142ecf2ee7f06
Adi Luthfi Nur Roki <adiluthfi03@gmail.com>
2022-11-17 16:33:54 +0700

    create class Node and declarate variable

--- Double_Linked_List/Program.cs
+++ Double_Linked_List/Program.cs
@@ -1,11 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Double_Linked_List
 {
+    class Node
+    {
+        /*Node class represents the node of doubly linked list.
+         * It consists of the information part and links to
+         * its succeeding and preceeding nodes
+         * in terms of next and previous nodes.*/
+        public int rollNumber;
+        public string name;
+        public Node next;/*point to the succeding node*/
+        public Node prev;/*point to the preceeding node*/
+    }
     internal class Program
     {
         static void Main(string[] args)
