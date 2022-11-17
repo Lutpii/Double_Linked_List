6fab7e4aed087dbaaa41f96ee920e46fd1308ac4
Adi Luthfi Nur Roki <adiluthfi03@gmail.com>
2022-11-17 16:48:34 +0700

    create class doublelinkedlist and add method addNode

--- Double_Linked_List/Program.cs
+++ Double_Linked_List/Program.cs
@@ -13,7 +13,68 @@ namespace Double_Linked_List
         public Node next;/*point to the succeding node*/
         public Node prev;/*point to the preceeding node*/
     }
-    internal class Program
+    class DoubleLinkedList
+    {
+        Node START;
+        public DoubleLinkedList()
+        {
+            START = null;
+        }
+        public void addNode()/*Adds a new node*/
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student: ");
+            nm = Console.ReadLine();
+            Node newNode = new Node();
+            newNode.rollNumber = rollNo;
+            newNode.name = nm;
+            /*Checks if the list is empty*/
+            if (START == null || rollNo <= START.rollNumber)
+            {
+                if ((START != null) && (rollNo == START.rollNumber))
+                {
+                    Console.WriteLine("\nDuplicate number not allowed");
+                    return;
+                }
+                newNode.next = START;
+                if (START != null)
+                    START.prev = newNode;
+                newNode.prev = null;
+                START = newNode;
+                return;
+            }
+            Node previous, current;
+            for (current = previous = START; current != null &&
+                rollNo >= current.rollNumber; previous = current, current =
+                current.next)
+            {
+                if (rollNo == current.rollNumber)
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+            }
+            /*Onthe execution of the above for loop, prev and
+             * current will point to those nodes
+             between which the new node is to be inserted.*/
+            newNode.next = current;
+            newNode.prev = previous;
+
+            /*If the node is to be inserted at the end of the list.*/
+            if (current == null)
+            {
+                newNode.next = null;
+                previous.next = newNode;
+                return;
+            }
+            current.prev = newNode;
+            current.next = newNode;
+        }
+    }
+        internal class Program
     {
         static void Main(string[] args)
         {
